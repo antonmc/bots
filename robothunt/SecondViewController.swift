cea0ba41544a103c0c95563e53de5f356a4ef74d
antonm <antonm@ca.ibm.com>
2015-12-27 16:09:37 -0500

    Initial Commit

---
+++ robothunt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  robothunt
+//
+//  Created by Anton McConville on 2015-12-27.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
